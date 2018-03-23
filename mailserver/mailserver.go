fb328df766c4c505c5da38c6379e42b2aabe6134
Topper Bowers <topper@toppingdesign.com>
2018-03-23 15:06:03 +0100

    mail server archive, ack, foreach

--- mailserver/mailserver.go
+++ mailserver/mailserver.go
@@ -7,14 +7,25 @@ import (
 	"github.com/quorumcontrol/qc3/storage"
 	"github.com/quorumcontrol/qc3/mailserver/mailserverpb"
 	"github.com/ethereum/go-ethereum/crypto"
+	whisper "github.com/ethereum/go-ethereum/whisper/whisperv5"
+	"bytes"
+	"github.com/ethereum/go-ethereum/rlp"
 )
 
 type MailServer struct {
 	storage storage.Storage
 }
 
+func NewMailServer(storage storage.Storage) *MailServer {
+	return &MailServer{storage: storage}
+}
+
 func NewDbKey(hsh common.Hash) []byte {
 	return hsh.Bytes()
+}
+
+func (ms *MailServer) Close() {
+	ms.storage.Close()
 }
 
 func (ms *MailServer) Archive(nestedEnvelope *mailserverpb.NestedEnvelope) error {
@@ -31,4 +42,14 @@ func (ms *MailServer) Archive(nestedEnvelope *mailserverpb.NestedEnvelope) error
 func (ms *MailServer) Ack(destination, envHash []byte) error {
 	key := NewDbKey(common.BytesToHash(envHash))
 	return ms.storage.Delete(destination, key)
+}
+
+func (ms *MailServer) ForEach(destination []byte, iteratorFunc func(env *whisper.Envelope) error) error {
+	return ms.storage.ForEach(destination, func(_,v []byte) error {
+		reader := bytes.NewReader(v)
+		stream := rlp.NewStream(reader, 10 * 1024 * 1024) // 10MB limit
+		env := &whisper.Envelope{}
+		env.DecodeRLP(stream)
+		return iteratorFunc(env)
+	})
 }
