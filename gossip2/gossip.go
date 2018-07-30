f5b1e010a6005c6f5c834818b0ecfe2dbd45e3f3
Topper Bowers <topper@toppingdesign.com>
2018-07-30 19:44:57 +0200

    the general layout of gossip2

---
+++ gossip2/gossip.go
@@ -0,0 +1,237 @@
+package gossip2
+
+import (
+	"context"
+	"time"
+
+	"crypto/ecdsa"
+
+	"sync"
+
+	"fmt"
+
+	"bytes"
+
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-ipld-cbor"
+	"github.com/quorumcontrol/chaintree/dag"
+	"github.com/quorumcontrol/qc3/bls"
+	"github.com/quorumcontrol/qc3/consensus"
+	"github.com/quorumcontrol/qc3/network"
+	"github.com/quorumcontrol/qc3/storage"
+)
+
+func init() {
+	cbornode.RegisterCborType(storedTransaction{})
+	cbornode.RegisterCborType(InitialMessage{})
+}
+
+const MessageType_Proposal = "PROPOSAL"
+const MessageType_Prepare = "PREPARE"
+const MessageType_TentativeCommit = "TENTATIVE_COMMIT"
+
+var currentStateBucket = []byte("CurrentState")
+var transactionBucket = []byte("Transactions")
+
+const DefaultRoundLength = 10 // round length in seconds
+const DefaultPhase1Length = 5 // round length in seconds
+const DefaultFanout = 3
+
+type ObjectId []byte
+type Transaction []byte
+type TransactionId []byte
+type ConflictSetId []byte
+type waitChanHolder map[string]chan bool
+
+type StoredCurrentState struct {
+	ObjectId  ObjectId
+	Tip       []byte
+	Signature consensus.Signature
+	Round     int64
+}
+
+type storedTransaction struct {
+	ObjectId    ObjectId
+	Transaction Transaction
+	PreviousTip []byte
+	NewTip      []byte
+	Round       int64
+}
+
+func (st *storedTransaction) Id() TransactionId {
+	return crypto.Keccak256(st.Transaction)
+}
+
+type storedRound struct {
+	TransactionId TransactionId
+	Phase         string
+	Signatures    *consensus.SignatureMap
+}
+
+type proposalMessage struct {
+	Transaction Transaction
+	ObjectId    ObjectId
+	PreviousTip []byte
+	Round       int64
+}
+
+type InitialMessage struct {
+	Transaction Transaction
+	ObjectId    ObjectId
+	PreviousTip []byte
+}
+
+type conflictSet struct {
+	Proposals []storedTransaction
+}
+
+type StateHandler func(ctx context.Context, group *consensus.Group, objectId, transaction, currentState []byte) (nextState []byte, accepted bool, err error)
+type AcceptedHandler func(ctx context.Context, group *consensus.Group, objectId, transaction, newState []byte) (err error)
+type RejectedHandler func(ctx context.Context, group *consensus.Group, objectId, transaction, currentState []byte) (err error)
+
+type handler interface {
+	DoRequest(dst *ecdsa.PublicKey, req *network.Request) (chan *network.Response, error)
+	AssignHandler(requestType string, handlerFunc network.HandlerFunc) error
+	Start()
+	Stop()
+}
+
+type Gossiper struct {
+	Id              string
+	SignKey         *bls.SignKey
+	Group           *consensus.Group
+	Fanout          int
+	RoundLength     int
+	Phase1Length    int
+	Storage         storage.Storage
+	MessageHandler  handler
+	StateHandler    StateHandler
+	AcceptedHandler AcceptedHandler
+	RejectedHandler RejectedHandler
+	waits           waitChanHolder
+	lock            *sync.RWMutex
+}
+
+func (g *Gossiper) Initialize() {
+	g.lock = &sync.RWMutex{}
+	g.Storage.CreateBucketIfNotExists(currentStateBucket)
+	g.Storage.CreateBucketIfNotExists(transactionBucket)
+	g.waits = make(waitChanHolder)
+
+	g.MessageHandler.AssignHandler(MessageType_Proposal, g.Propose)
+	if g.Id == "" {
+		g.Id = consensus.BlsVerKeyToAddress(g.SignKey.MustVerKey().Bytes()).String()
+	}
+	if g.RoundLength == 0 {
+		g.RoundLength = DefaultRoundLength
+	}
+	if g.Fanout == 0 {
+		g.Fanout = DefaultFanout // good for approx 1k - 9k nodes
+	}
+	if g.Phase1Length == 0 {
+		g.Phase1Length = DefaultPhase1Length
+	}
+}
+
+func conflictSetId(objectId ObjectId, previousTip *cid.Cid) []byte {
+	return append(objectId, previousTip.Bytes()...)
+}
+
+// return the round number (rounds are set to 10 seconds)
+func (g *Gossiper) roundAt(t time.Time) int64 {
+	return t.UTC().Unix() / int64(g.RoundLength)
+}
+
+func (g *Gossiper) Propose(ctx context.Context, req network.Request, respChan network.ResponseChan) error {
+	msg := &InitialMessage{}
+	err := cbornode.DecodeInto(req.Payload, msg)
+	if err != nil {
+		return fmt.Errorf("error decoding: %v", err)
+	}
+
+	currState, err := g.GetCurrenState(ctx, msg.ObjectId)
+	if err != nil {
+		return fmt.Errorf("error getting current state: %v", err)
+	}
+
+	if !bytes.Equal(msg.PreviousTip, currState.Tip) {
+		return fmt.Errorf("invalid previous tip: %s, currently: %s", msg.PreviousTip, currState.Tip)
+	}
+
+	handlerState, isAccepted, err := g.StateHandler(ctx, g.Group, msg.ObjectId, msg.Transaction, currState.Tip)
+	if err != nil {
+		return fmt.Errorf("error calling state handler: %v", err)
+	}
+
+	if !isAccepted {
+		return fmt.Errorf("not accepted")
+	}
+
+	toStore := &storedTransaction{
+		ObjectId:    msg.ObjectId,
+		Transaction: msg.Transaction,
+		PreviousTip: msg.PreviousTip,
+		NewTip:      handlerState,
+		Round:       g.roundAt(time.Now()),
+	}
+
+	err = g.saveTransaction(toStore)
+	if err != nil {
+		return fmt.Errorf("error saving transaction")
+	}
+
+	go func() {
+		time.Sleep(time.Duration(g.Phase1Length) * time.Second)
+		// then prepare it
+	}()
+
+	//gossip the msg
+
+	resp, err := network.BuildResponse(req.Id, 200, true)
+	if err != nil {
+		return fmt.Errorf("error building response: %v", err)
+	}
+	respChan <- resp
+
+	return err
+}
+
+func (g *Gossiper) HandleTransactionGossip(ctx context.Context, req network.Request, respChan network.ResponseChan) error {
+
+	return nil
+}
+
+func (g *Gossiper) HandlePrepare(ctx context.Context, req network.Request, respChan network.ResponseChan) error {
+	return nil
+}
+
+func (g *Gossiper) GetCurrenState(ctx context.Context, objectId ObjectId) (state StoredCurrentState, err error) {
+	g.lock.RLock()
+	defer g.lock.RUnlock()
+
+	storedBytes, err := g.Storage.Get(currentStateBucket, objectId)
+	if err != nil {
+		return state, fmt.Errorf("error getting state: %v", err)
+	}
+	if len(storedBytes) == 0 {
+		return state, nil
+	}
+
+	err = cbornode.DecodeInto(storedBytes, state)
+	if err != nil {
+		return state, fmt.Errorf("error decoding: %v", err)
+	}
+
+	return state, err
+}
+
+func (g *Gossiper) saveTransaction(st *storedTransaction) error {
+	sw := dag.SafeWrap{}
+	cborNode := sw.WrapObject(st)
+	if sw.Err != nil {
+		return fmt.Errorf("error wrapping: %v", sw.Err)
+	}
+
+	return g.Storage.Set(transactionBucket, st.Id(), cborNode.RawData())
+}
---
+++ gossip2/gossip_test.go
@@ -0,0 +1,216 @@
+package gossip2
+
+import (
+	"bytes"
+	"context"
+	"crypto/ecdsa"
+	"testing"
+	"time"
+
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/log"
+	"github.com/ipfs/go-ipld-cbor"
+	"github.com/quorumcontrol/qc3/bls"
+	"github.com/quorumcontrol/qc3/consensus"
+	"github.com/quorumcontrol/qc3/network"
+	"github.com/quorumcontrol/qc3/storage"
+	"github.com/stretchr/testify/assert"
+)
+
+var lastAccepted []byte
+var lastRejected []byte
+
+type testSet struct {
+	SignKeys          []*bls.SignKey
+	VerKeys           []*bls.VerKey
+	EcdsaKeys         []*ecdsa.PrivateKey
+	PubKeys           []consensus.PublicKey
+	SignKeysByAddress map[string]*bls.SignKey
+}
+
+// This is the simplest possible state handler that just always returns the transaction as the nextState
+func simpleHandler(_ context.Context, _ *consensus.Group, _, transaction, _ []byte) (nextState []byte, accepted bool, err error) {
+	if bytes.HasPrefix(transaction, []byte("reject")) {
+		log.Debug("rejecting transaction")
+		return []byte("bad state no use"), false, nil
+	}
+	return transaction, true, nil
+}
+
+func simpleAcceptance(_ context.Context, _ *consensus.Group, _, transaction, _ []byte) (err error) {
+	log.Debug("simpleAcceptance called")
+	lastAccepted = transaction
+	return nil
+}
+
+func simpleRejecter(ctx context.Context, group *consensus.Group, objectId, transaction, currentState []byte) (err error) {
+	log.Debug("simpleRejecter called")
+	lastRejected = transaction
+	return nil
+}
+
+type InMemoryHandlerSystem struct {
+	Handlers          map[string]*InMemoryHandler
+	ArtificialLatency int
+}
+
+func NewInMemoryHandlerSystem(latency int) *InMemoryHandlerSystem {
+	return &InMemoryHandlerSystem{
+		Handlers:          make(map[string]*InMemoryHandler),
+		ArtificialLatency: latency,
+	}
+}
+
+func (imhs *InMemoryHandlerSystem) NewHandler(dst *ecdsa.PublicKey) *InMemoryHandler {
+	imh := &InMemoryHandler{
+		Id:     crypto.PubkeyToAddress(*dst).String(),
+		System: imhs,
+	}
+	imhs.Handlers[imh.Id] = imh
+	return imh
+}
+
+type InMemoryHandler struct {
+	Id      string
+	System  *InMemoryHandlerSystem
+	Mapping map[string]network.HandlerFunc
+}
+
+func (imh *InMemoryHandler) DoRequest(dst *ecdsa.PublicKey, req *network.Request) (chan *network.Response, error) {
+	respChan := make(chan *network.Response, 1)
+	start := time.Now()
+
+	log.Trace("DoRequest to", "dst", crypto.PubkeyToAddress(*dst).String(), "uuid", req.Id, "start", start)
+	go func(dst *ecdsa.PublicKey, req *network.Request) {
+		log.Trace("DoRequest go func started", "dst", crypto.PubkeyToAddress(*dst).String(), "uuid", req.Id, "elapsed", time.Now().Sub(start))
+		handler, ok := imh.System.Handlers[crypto.PubkeyToAddress(*dst).String()]
+		if !ok {
+			log.Error("could not find handler")
+			panic("could not find handler")
+		}
+		internalRespChan := make(network.ResponseChan, 1)
+		defer close(respChan)
+
+		err := handler.Mapping[req.Type](context.Background(), *req, internalRespChan)
+		if err != nil {
+			log.Error("error handling request: %v", err)
+		}
+
+		log.Trace("DoRequest func executed", "dst", crypto.PubkeyToAddress(*dst).String(), "uuid", req.Id, "elapsed", time.Now().Sub(start))
+		<-time.After(time.Duration(imh.System.ArtificialLatency) * time.Millisecond)
+		log.Trace("DoRequest responding", "dst", crypto.PubkeyToAddress(*dst).String(), "uuid", req.Id, "elapsed", time.Now().Sub(start))
+		respChan <- <-internalRespChan
+	}(dst, req)
+
+	return respChan, nil
+}
+
+func (imh *InMemoryHandler) AssignHandler(requestType string, handlerFunc network.HandlerFunc) error {
+	if imh.Mapping == nil {
+		imh.Mapping = make(map[string]network.HandlerFunc)
+	}
+
+	imh.Mapping[requestType] = handlerFunc
+	return nil
+}
+
+func (imh *InMemoryHandler) Start() {}
+func (imh *InMemoryHandler) Stop()  {}
+
+func newTestSet(t *testing.T, size int) *testSet {
+	signKeys := blsKeys(size)
+	verKeys := make([]*bls.VerKey, len(signKeys))
+	pubKeys := make([]consensus.PublicKey, len(signKeys))
+	ecdsaKeys := make([]*ecdsa.PrivateKey, len(signKeys))
+	signKeysByAddress := make(map[string]*bls.SignKey)
+	for i, signKey := range signKeys {
+		ecdsaKey, _ := crypto.GenerateKey()
+		verKeys[i] = signKey.MustVerKey()
+		pubKeys[i] = consensus.BlsKeyToPublicKey(verKeys[i])
+		ecdsaKeys[i] = ecdsaKey
+		signKeysByAddress[consensus.BlsVerKeyToAddress(verKeys[i].Bytes()).String()] = signKey
+
+	}
+
+	return &testSet{
+		SignKeys:          signKeys,
+		VerKeys:           verKeys,
+		PubKeys:           pubKeys,
+		EcdsaKeys:         ecdsaKeys,
+		SignKeysByAddress: signKeysByAddress,
+	}
+}
+
+func groupFromTestSet(t *testing.T, set *testSet) *consensus.Group {
+	members := make([]*consensus.RemoteNode, len(set.SignKeys))
+	for i := range set.SignKeys {
+		rn := consensus.NewRemoteNode(consensus.BlsKeyToPublicKey(set.VerKeys[i]), consensus.EcdsaToPublicKey(&set.EcdsaKeys[i].PublicKey))
+		members[i] = rn
+	}
+
+	return consensus.NewGroup(members)
+}
+
+func blsKeys(size int) []*bls.SignKey {
+	keys := make([]*bls.SignKey, size)
+	for i := 0; i < size; i++ {
+		keys[i] = bls.MustNewSignKey()
+	}
+	return keys
+}
+
+func generateTestGossipGroup(t *testing.T, size int, latency int) []*Gossiper {
+	ts := newTestSet(t, size)
+	group := groupFromTestSet(t, ts)
+
+	system := NewInMemoryHandlerSystem(latency)
+
+	gossipers := make([]*Gossiper, size)
+
+	for i := 0; i < size; i++ {
+		stor := storage.NewMemStorage()
+
+		member := group.SortedMembers[i]
+
+		gossiper := &Gossiper{
+			SignKey:         ts.SignKeysByAddress[member.Id],
+			Storage:         stor,
+			StateHandler:    simpleHandler,
+			AcceptedHandler: simpleAcceptance,
+			RejectedHandler: simpleRejecter,
+			Group:           group,
+			MessageHandler:  system.NewHandler(crypto.ToECDSAPub(member.DstKey.PublicKey)),
+		}
+
+		gossiper.Initialize()
+		gossipers[i] = gossiper
+	}
+
+	return gossipers
+}
+
+func TestGossiper_Propose(t *testing.T) {
+	gossipers := generateTestGossipGroup(t, 1, 0)
+
+	message := &InitialMessage{
+		ObjectId:    []byte("obj"),
+		Transaction: []byte("trans"),
+		PreviousTip: []byte{},
+	}
+
+	req, err := network.BuildRequest(MessageType_Proposal, message)
+	assert.Nil(t, err)
+
+	respChan := make(network.ResponseChan, 1)
+
+	err = gossipers[0].Propose(context.TODO(), *req, respChan)
+	assert.Nil(t, err)
+
+	resp := <-respChan
+
+	gossipResp := new(bool)
+	err = cbornode.DecodeInto(resp.Payload, gossipResp)
+	assert.Nil(t, err)
+
+	assert.True(t, *gossipResp)
+}
