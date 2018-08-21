207c7bc62eb0161c01f373426d04393aed90a512
Topper Bowers <topper@toppingdesign.com>
2018-08-21 11:51:37 +0200

    add a genesis state creator to the notary group

--- consensus/notarygroup_test.go
+++ consensus/notarygroup_test.go
@@ -2,6 +2,7 @@ package consensus
 
 import (
 	"testing"
+	"time"
 
 	"github.com/quorumcontrol/qc3/bls"
 
@@ -100,4 +101,27 @@ func TestNotaryGroupMostRecentRoundInfo(t *testing.T) {
 	roundInfo, err := group.MostRecentRoundInfo(5)
 	require.Nil(t, err)
 	assert.Equal(t, []*RemoteNode{rn}, roundInfo.Signers)
+}
+
+func TestNotaryGroupCreateGenesisState(t *testing.T) {
+	nodeStore := nodestore.NewStorageBasedStore(storage.NewMemStorage())
+
+	key, err := crypto.GenerateKey()
+	require.Nil(t, err)
+	id := AddrToDid(crypto.PubkeyToAddress(key.PublicKey).String())
+	group := NewNotaryGroup(id, nodeStore)
+
+	rnDstKey, err := crypto.GenerateKey()
+	rnKey := bls.MustNewSignKey()
+
+	round := group.RoundAt(time.Now())
+
+	rn := NewRemoteNode(BlsKeyToPublicKey(rnKey.MustVerKey()), EcdsaToPublicKey(&rnDstKey.PublicKey))
+	group.CreateGenesisState(round, rn)
+
+	for i := 0; i <= 8; i++ {
+		roundInfo, err := group.RoundInfoFor(round + int64(i))
+		require.Nil(t, err)
+		assert.Equal(t, []*RemoteNode{rn}, roundInfo.Signers, "round %d", round+int64(i))
+	}
 }
