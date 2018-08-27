4b49a3ac0ec65f3be0e7f77087d716a4cec7c837
Brandon Westcott <brandon@quorumcontrol.com>
2018-08-27 16:59:21 -0400

    Switch back log line from debug to trace

--- network/messagehandler.go
+++ network/messagehandler.go
@@ -183,7 +183,7 @@ func (rh *MessageHandler) Broadcast(topic, symKey []byte, req *Request) error {
 		return fmt.Errorf("error wrapping request: %v", sw.Err)
 	}
 
-	log.Debug("broadcast message", "id", req.Id, "source", crypto.PubkeyToAddress(rh.node.key.PublicKey).String())
+	log.Trace("broadcast message", "id", req.Id, "source", crypto.PubkeyToAddress(rh.node.key.PublicKey).String())
 	err := rh.node.Send(MessageParams{
 		Payload:  reqNode.RawData(),
 		TTL:      DefaultTTL,
