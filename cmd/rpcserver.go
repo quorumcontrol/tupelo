bc6486dfa0929506f1acdc18af3fec97ceda1cc1
ben lamothe <bl@732.one>
2018-10-02 00:45:40 -0400

    start a local test net with the rpc server

--- cmd/rpcserver.go
+++ cmd/rpcserver.go
@@ -1,33 +1,36 @@
 package cmd
 
 import (
-	"fmt"
-	"time"
-
-	"github.com/quorumcontrol/chaintree/nodestore"
-	"github.com/quorumcontrol/qc3/consensus"
+	"github.com/quorumcontrol/qc3/signer"
 	"github.com/quorumcontrol/qc3/wallet/walletrpc"
-	"github.com/quorumcontrol/storage"
 	"github.com/spf13/cobra"
 )
+
+var bootstrapPrivateKeysFile string
 
 var rpcServerCmd = &cobra.Command{
 	Use:   "rpc-server",
 	Short: "Launches a Tupelo RPC Server",
 	Run: func(cmd *cobra.Command, args []string) {
-		memStore := nodestore.NewStorageBasedStore(storage.NewMemStorage())
-		testNetGroup := consensus.NewNotaryGroup("hardcodedprivatekeysareunsafe", memStore)
-		if testNetGroup.IsGenesis() {
-			testNetMembers := bootstrapMembers(bootstrapKeysFile)
-			fmt.Printf("Bootstrapping notary group with %v nodes\n", len(testNetMembers))
-			testNetGroup.CreateGenesisState(testNetGroup.RoundAt(time.Now()), testNetMembers...)
+		// memStore := nodestore.NewStorageBasedStore(storage.NewMemStorage())
+		// notaryGroup := consensus.NewNotaryGroup("hardcodedprivatekeysareunsafe", memStore)
+		// if notaryGroup.IsGenesis() {
+		//	testNetMembers := bootstrapMembers(bootstrapPublicKeysFile)
+		//	fmt.Printf("Bootstrapping notary group with %v nodes\n", len(testNetMembers))
+		//	notaryGroup.CreateGenesisState(notaryGroup.RoundAt(time.Now()), testNetMembers...)
+		// }
+		notaryGroup := setupNotaryGroup()
+		privateKeys, _ := loadPrivateKeyFile(bootstrapPrivateKeysFile)
+		signers := make([]*signer.GossipedSigner, len(privateKeys))
+		for i, keys := range privateKeys {
+			signers[i] = setupGossipNode(keys.EcdsaHexPrivateKey, keys.BlsHexPrivateKey, notaryGroup)
 		}
-
-		walletrpc.Serve(testNetGroup)
+		walletrpc.Serve(notaryGroup)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(rpcServerCmd)
-	rpcServerCmd.Flags().StringVarP(&bootstrapKeysFile, "bootstrap-keys", "k", "", "which keys to bootstrap the notary groups with")
+	rpcServerCmd.Flags().StringVarP(&bootstrapPublicKeysFile, "bootstrap-keys", "k", "", "which public keys to bootstrap the notary groups with")
+	rpcServerCmd.Flags().StringVarP(&bootstrapPrivateKeysFile, "bootstrap-private-keys", "s", "", "which private keys to bootstrap the notary groups with")
 }
