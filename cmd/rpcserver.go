7ac2efba048264ef8cc60ea070b5e21d4823db21
ben lamothe <bl@732.one>
2018-10-07 12:18:12 -0400

    add option to serve RPC over TLS

--- cmd/rpcserver.go
+++ cmd/rpcserver.go
@@ -2,13 +2,20 @@ package cmd
 
 import (
 	"encoding/json"
+	"strings"
 
 	"github.com/quorumcontrol/qc3/signer"
 	"github.com/quorumcontrol/qc3/wallet/walletrpc"
 	"github.com/spf13/cobra"
 )
 
-var bootstrapPrivateKeysFile string
+var (
+	bootstrapPrivateKeysFile string
+	tls                      bool
+	certFile                 string
+	keyFile                  string
+)
+
 func loadPrivateKeyFile(path string) ([]*PrivateKeySet, error) {
 	var jsonLoadedKeys []*PrivateKeySet
 
@@ -25,6 +32,23 @@ func loadPrivateKeyFile(path string) ([]*PrivateKeySet, error) {
 	return jsonLoadedKeys, nil
 }
 
+func panicWithoutTLSOpts() {
+	if certFile == "" || keyFile == "" {
+		var msg strings.Builder
+		if certFile == "" {
+			msg.WriteString("Missing certificate file path. ")
+			msg.WriteString("Please supply with the -C flag. ")
+		}
+
+		if keyFile == "" {
+			msg.WriteString("Missing key file path. ")
+			msg.WriteString("Please supply with the -K flag. ")
+		}
+
+		panic(msg.String())
+	}
+}
+
 var rpcServerCmd = &cobra.Command{
 	Use:   "rpc-server",
 	Short: "Launches a Tupelo RPC Server",
@@ -35,7 +59,13 @@ var rpcServerCmd = &cobra.Command{
 		for i, keys := range privateKeys {
 			signers[i] = setupGossipNode(keys.EcdsaHexPrivateKey, keys.BlsHexPrivateKey, notaryGroup)
 		}
-		walletrpc.Serve(notaryGroup)
+		if tls {
+			panicWithoutTLSOpts()
+
+			walletrpc.ServeTLS(notaryGroup, certFile, keyFile)
+		} else {
+			walletrpc.ServeInsecure(notaryGroup)
+		}
 	},
 }
 
@@ -43,4 +73,7 @@ func init() {
 	rootCmd.AddCommand(rpcServerCmd)
 	rpcServerCmd.Flags().StringVarP(&bootstrapPublicKeysFile, "bootstrap-keys", "k", "", "which public keys to bootstrap the notary groups with")
 	rpcServerCmd.Flags().StringVarP(&bootstrapPrivateKeysFile, "bootstrap-private-keys", "s", "", "which private keys to bootstrap the notary groups with")
+	rpcServerCmd.Flags().BoolVarP(&tls, "tls", "t", false, "Encrypt connections with TLS/SSL")
+	rpcServerCmd.Flags().StringVarP(&certFile, "tls-cert", "C", "", "TLS certificate file")
+	rpcServerCmd.Flags().StringVarP(&keyFile, "tls-key", "K", "", "TLS private key file")
 }
