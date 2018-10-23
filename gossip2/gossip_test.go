493ad64970f27c8ed3da22e1cf1b5c125f53ea52
Topper Bowers <topper@toppingdesign.com>
2018-10-23 16:17:57 +0200

    passing tests for gossip2 passing an IBF

--- gossip2/gossip_test.go
+++ gossip2/gossip_test.go
@@ -1,12 +1,128 @@
 package gossip2
 
 import (
+	"context"
+	"crypto/ecdsa"
+	"os"
+	"strconv"
+	"strings"
 	"testing"
+	"time"
 
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/quorumcontrol/chaintree/nodestore"
 	"github.com/quorumcontrol/differencedigest/ibf"
+	"github.com/quorumcontrol/qc3/bls"
+	"github.com/quorumcontrol/qc3/consensus"
+	"github.com/quorumcontrol/qc3/p2p"
+	"github.com/quorumcontrol/storage"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
+
+func newBootstrapHost(ctx context.Context, t *testing.T) *p2p.Host {
+	key, err := crypto.GenerateKey()
+	require.Nil(t, err)
+
+	host, err := p2p.NewHost(ctx, key, p2p.GetRandomUnusedPort())
+
+	require.Nil(t, err)
+	require.NotNil(t, host)
+
+	return host
+}
+
+func bootstrapAddresses(bootstrapHost *p2p.Host) []string {
+	addresses := bootstrapHost.Addresses()
+	for _, addr := range addresses {
+		addrStr := addr.String()
+		if strings.Contains(addrStr, "127.0.0.1") {
+			return []string{addrStr}
+		}
+	}
+	return nil
+}
+
+type testSet struct {
+	SignKeys          []*bls.SignKey
+	VerKeys           []*bls.VerKey
+	EcdsaKeys         []*ecdsa.PrivateKey
+	PubKeys           []consensus.PublicKey
+	SignKeysByAddress map[string]*bls.SignKey
+}
+
+func newTestSet(t *testing.T, size int) *testSet {
+	signKeys := blsKeys(size)
+	verKeys := make([]*bls.VerKey, len(signKeys))
+	pubKeys := make([]consensus.PublicKey, len(signKeys))
+	ecdsaKeys := make([]*ecdsa.PrivateKey, len(signKeys))
+	signKeysByAddress := make(map[string]*bls.SignKey)
+	for i, signKey := range signKeys {
+		ecdsaKey, _ := crypto.GenerateKey()
+		verKeys[i] = signKey.MustVerKey()
+		pubKeys[i] = consensus.BlsKeyToPublicKey(verKeys[i])
+		ecdsaKeys[i] = ecdsaKey
+		signKeysByAddress[consensus.BlsVerKeyToAddress(verKeys[i].Bytes()).String()] = signKey
+
+	}
+
+	return &testSet{
+		SignKeys:          signKeys,
+		VerKeys:           verKeys,
+		PubKeys:           pubKeys,
+		EcdsaKeys:         ecdsaKeys,
+		SignKeysByAddress: signKeysByAddress,
+	}
+}
+
+func groupFromTestSet(t *testing.T, set *testSet) *consensus.NotaryGroup {
+	members := make([]*consensus.RemoteNode, len(set.SignKeys))
+	for i := range set.SignKeys {
+		rn := consensus.NewRemoteNode(consensus.BlsKeyToPublicKey(set.VerKeys[i]), consensus.EcdsaToPublicKey(&set.EcdsaKeys[i].PublicKey))
+		members[i] = rn
+	}
+
+	nodeStore := nodestore.NewStorageBasedStore(storage.NewMemStorage())
+	group := consensus.NewNotaryGroup("notarygroupid", nodeStore)
+	err := group.CreateGenesisState(group.RoundAt(time.Now()), members...)
+	require.Nil(t, err)
+	return group
+}
+
+func blsKeys(size int) []*bls.SignKey {
+	keys := make([]*bls.SignKey, size)
+	for i := 0; i < size; i++ {
+		keys[i] = bls.MustNewSignKey()
+	}
+	return keys
+}
+
+func TestGossip(t *testing.T) {
+	groupSize := 5
+	ts := newTestSet(t, groupSize)
+	group := groupFromTestSet(t, ts)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	bootstrap := newBootstrapHost(ctx, t)
+
+	gossipNodes := make([]*GossipNode, groupSize)
+
+	for i := 0; i < groupSize; i++ {
+		host, err := p2p.NewHost(ctx, ts.EcdsaKeys[i], p2p.GetRandomUnusedPort())
+		require.Nil(t, err)
+		host.Bootstrap(bootstrapAddresses(bootstrap))
+		path := "test/badger/" + strconv.Itoa(i)
+		os.MkdirAll(path, 0755)
+		storage := NewBadgerStorage(path)
+		gossipNodes[i] = NewGossipNode(ts.EcdsaKeys[i], host, storage)
+		gossipNodes[i].Group = group
+	}
+	time.Sleep(5 * time.Second)
+	err := gossipNodes[0].DoSync()
+	require.Nil(t, err)
+	time.Sleep(5 * time.Second)
+}
 
 func TestSanitySizes(t *testing.T) {
 	filter := ibf.NewInvertibleBloomFilter(2000, 4)
