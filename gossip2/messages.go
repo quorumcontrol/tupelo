3cb9444f27387c27a556e8dc74fc63c39b6b3a69
Topper Bowers <topper@toppingdesign.com>
2018-10-23 16:48:58 +0200

    passing the objects we want now works

--- gossip2/messages.go
+++ gossip2/messages.go
@@ -1,6 +1,10 @@
 //go:generate msgp
 package gossip2
 
+import (
+	"github.com/quorumcontrol/differencedigest/ibf"
+)
+
 type ConflictSet struct {
 	ObjectID []byte
 	Tip      []byte
@@ -23,7 +27,16 @@ type WantMessage struct {
 	Keys []uint64
 }
 
+func WantMessageFromDiff(objs []ibf.ObjectId) *WantMessage {
+	ints := make([]uint64, len(objs))
+	for i, objId := range objs {
+		ints[i] = uint64(objId)
+	}
+	return &WantMessage{Keys: ints}
+}
+
 type ProvideMessage struct {
 	Key   []byte
 	Value []byte
+	Last  bool
 }
