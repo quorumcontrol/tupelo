d60aa23fed69bfc817a8f132f7bdeec0941caa05
Brandon Westcott <brandon@quorumcontrol.com>
2018-10-24 13:53:11 -0400

    Fix transactionIDFromSignatureKey

--- gossip2/gossip_test.go
+++ gossip2/gossip_test.go
@@ -172,3 +172,17 @@ func TestGossip(t *testing.T) {
 	}
 	assert.True(t, matchedOne)
 }
+
+func TestTransactionIDFromSignatureKey(t *testing.T) {
+	transaction := Transaction{
+		ObjectID:    []byte("himynameisalongobjectidthatwillhavemorethan64bits"),
+		PreviousTip: []byte(""),
+		NewTip:      []byte("zdpuAs5LQAGsXbGTF3DbfGVkRw4sWJd4MzbbigtJ4zE6NNJrr"),
+		Payload:     []byte("thisisthepayload"),
+	}
+	signature := Signature{
+		TransactionID: transaction.ID(),
+	}
+
+	assert.Equal(t, transaction.StoredID(), transactionIDFromSignatureKey(signature.StoredID(transaction.ToConflictSet().ID())))
+}
