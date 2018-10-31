3c97fc1c0f7ccdec197da5737c23f4d99dcdc116
Topper Bowers <topper@toppingdesign.com>
2018-10-31 10:46:46 +0100

    Add back in the 503 handler for too many syncs

--- gossip2/messages.go
+++ gossip2/messages.go
@@ -2,6 +2,8 @@
 package gossip2
 
 import (
+	"fmt"
+
 	"github.com/quorumcontrol/differencedigest/ibf"
 )
 
@@ -27,6 +29,20 @@ type Signature struct {
 	TransactionID []byte
 	Signers       map[string]bool
 	Signature     []byte
+}
+
+const (
+	ProtocolTypeWants int = iota
+	ProtocolTypeError
+	ProtocolTypeIBF
+	ProtocolTypeStrata
+)
+
+type ProtocolMessage struct {
+	Code        int
+	Error       string
+	MessageType int
+	Payload     []byte
 }
 
 type WantMessage struct {
@@ -60,4 +76,49 @@ type ConflictSetQuery struct {
 type ConflictSetQueryResponse struct {
 	Key  []byte
 	Done bool
+}
+
+type msgPackObject interface {
+	MarshalMsg([]byte) ([]byte, error)
+	UnmarshalMsg([]byte) ([]byte, error)
+}
+
+func toProtocolMessage(msg msgPackObject) (ProtocolMessage, error) {
+
+	oBytes, err := msg.MarshalMsg(nil)
+	if err != nil {
+		return ProtocolMessage{}, fmt.Errorf("error marshaling message: %v", err)
+	}
+	pm := ProtocolMessage{
+		Payload: oBytes,
+	}
+
+	switch t := msg.(type) {
+	case *WantMessage:
+		pm.MessageType = ProtocolTypeWants
+	case *ibf.InvertibleBloomFilter:
+		pm.MessageType = ProtocolTypeIBF
+	case *ibf.DifferenceStrata:
+		pm.MessageType = ProtocolTypeIBF
+	default:
+		return ProtocolMessage{}, fmt.Errorf("unknown type: %v", t)
+	}
+
+	return pm, nil
+}
+
+func fromProtocolMessage(pm *ProtocolMessage) (msgPackObject, error) {
+	var out msgPackObject
+	switch pm.MessageType {
+	case ProtocolTypeWants:
+		out = &WantMessage{}
+	case ProtocolTypeIBF:
+		out = &ibf.InvertibleBloomFilter{}
+	case ProtocolTypeStrata:
+		out = &ibf.DifferenceStrata{}
+	default:
+		return nil, fmt.Errorf("unknown type code: %v", pm.MessageType)
+	}
+	_, err := out.UnmarshalMsg(pm.Payload)
+	return out, err
 }
