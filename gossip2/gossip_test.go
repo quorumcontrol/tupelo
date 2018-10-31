b8377c879a7c17ec4587fa340163c840d6458c6c
Topper Bowers <topper@toppingdesign.com>
2018-10-31 08:59:45 +0100

    commit the commented out pprof

--- gossip2/gossip_test.go
+++ gossip2/gossip_test.go
@@ -6,7 +6,6 @@ import (
 	"log"
 	"math/rand"
 	"os"
-	"runtime/pprof"
 	"strconv"
 	"strings"
 	"sync/atomic"
@@ -173,12 +172,12 @@ func TestGossip(t *testing.T) {
 	}
 
 	// This bit of commented out code will run the CPU profiler
-	f, ferr := os.Create("../gossip.prof")
-	if ferr != nil {
-		t.Fatal(ferr)
-	}
-	pprof.StartCPUProfile(f)
-	defer pprof.StopCPUProfile()
+	// f, ferr := os.Create("../gossip.prof")
+	// if ferr != nil {
+	// 	t.Fatal(ferr)
+	// }
+	// pprof.StartCPUProfile(f)
+	// defer pprof.StopCPUProfile()
 
 	// for i := 0; i < 50; i++ {
 	// 	_, err := gossipNodes[rand.Intn(len(gossipNodes))].InitiateTransaction(Transaction{
