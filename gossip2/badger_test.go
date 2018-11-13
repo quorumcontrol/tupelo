1c72bfe6a58a72fc103733f0f47204e538e1af22
Brandon Westcott <brandon@quorumcontrol.com>
2018-11-13 15:37:00 -0500

    set default storage path in non integration test

--- gossip2/badger_test.go
+++ gossip2/badger_test.go
@@ -8,6 +8,8 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+const testStoragePath = "../.tmp/test/"
+
 func TestSetIfExists(t *testing.T) {
 	path := testStoragePath + "setIfExistsTest"
 	os.RemoveAll(path)
