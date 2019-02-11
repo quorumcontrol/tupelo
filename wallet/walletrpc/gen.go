672bffaa606adf5db7a8fc041130b48a08de3c28
ben lamothe <bl@732.one>
2019-02-11 09:19:48 -0500

    add http annotations

--- wallet/walletrpc/gen.go
+++ wallet/walletrpc/gen.go
@@ -1,5 +1,5 @@
 package walletrpc
 
-//go:generate protoc -I=. -I=$GOPATH/src --go_out=plugins=grpc:. service.proto
+//go:generate protoc -I. -I$GOPATH/src -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis --go_out=plugins=grpc:. --grpc-gateway_out=logtostderr=true:. service.proto
 
-// RUN `go generate` in this directory when updating the service.proto. Requires the protoc command to be in your path.
+//RUN `go generate` in this directory when updating the service.proto. Requires the protoc command to be in your path.
