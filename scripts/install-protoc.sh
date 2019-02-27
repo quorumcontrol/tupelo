ff0cd7e361488d568b96babf3308385c31953aca
Wes Morgan <wesmorgan@icloud.com>
2019-02-27 13:36:54 -0700

    Make protoc install script even less verbose & fix typo

--- scripts/install-protoc.sh
+++ scripts/install-protoc.sh
@@ -4,19 +4,22 @@ set -e
 
 PROTOC_VERSION=3.6.1
 
-sudo apt-get update
-sudo apt-get install -y autoconf automake libtool curl make g++ unzip
+echo "Installing protoc build deps"
+sudo apt-get update > /dev/null
+sudo apt-get install -y autoconf automake libtool curl make g++ unzip > /dev/null
 
-curl -LO https://github.com/protocolbuffers/protobuf/releases/download/v${PROTOC_VERSION}/protobuf-all-${PROTOC_VERSION}.tar.gz
+echo "Downloading protoc source code"
+curl -LO https://github.com/protocolbuffers/protobuf/releases/download/v${PROTOC_VERSION}/protobuf-all-${PROTOC_VERSION}.tar.gz > /dev/null
 
-tar -xzvf protobuf-all-${PROTOC_VERSION}.tar.gz
+echo "Extracting protoc source code"
+tar -xzvf protobuf-all-${PROTOC_VERSION}.tar.gz > /dev/null
 
 cd protobuf-${PROTOC_VERSION}
 
 echo "Configuring protoc"
 ./configure > /dev/null
 echo "Building protoc"
-make > / dev/null
+make > /dev/null
 echo "Installing protoc"
 sudo make install > /dev/null
 sudo ldconfig > /dev/null
