8d6ae16a894dab7550a37a148b148d4e2ec7973b
Topper Bowers <topper@toppingdesign.com>
2019-03-13 16:29:58 +0100

    log errors when a bad push synced comes back

--- gossip3/actors/gossiper.go
+++ gossip3/actors/gossiper.go
@@ -64,7 +64,6 @@ func (g *Gossiper) Receive(context actor.Context) {
 	switch msg := context.Message().(type) {
 	case *actor.Restarting:
 		g.Log.Infow("restarting")
-	case *actor.Started:
 	case *actor.Terminated:
 		// this is for when the pushers stop, we can queue up another push
 		if _, ok := g.pids[currentPusherKey]; ok && msg.Who.Equal(g.pids[currentPusherKey]) {
@@ -83,17 +82,19 @@ func (g *Gossiper) Receive(context actor.Context) {
 			g.syncersAvailable++
 			return
 		}
+		g.Log.Errorw("unknown actor terminated", "who", msg.Who.GetId(), "pids", g.pids)
+
 		panic(fmt.Sprintf("unknown actor terminated: %s", msg.Who.GetId()))
 
 	case *messages.StartGossip:
-		g.Log.Infow("start gossip")
+		g.Log.Debugw("start gossip")
 		g.validatorClear = true
 		context.Self().Tell(&messages.DoOneGossip{
 			Why: "startGosip",
 		})
 	case *messages.DoOneGossip:
 		if _, ok := g.pids[currentPusherKey]; ok {
-			g.Log.Infow("ignoring because in progress")
+			g.Log.Debugw("ignoring because in progress")
 			return
 		}
 		g.Log.Debugw("gossiping again")
