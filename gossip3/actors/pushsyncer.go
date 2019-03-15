8adb6c764807684d3d708fb39a3e6c862b4566fc
Wes Morgan <wesmorgan@icloud.com>
2019-03-15 13:19:00 -0600

    Increase remote syncer timeout to 120s
    I don't see timeouts even after several test runs with this value. Still not great, though.

--- gossip3/actors/pushsyncer.go
+++ gossip3/actors/pushsyncer.go
@@ -97,7 +97,7 @@ func (syncer *PushSyncer) handleDoPush(context actor.Context, msg *messages.DoPu
 
 	resp, err := context.RequestFuture(remoteGossiper, &messages.GetSyncer{
 		Kind: syncer.kind,
-	}, 60*time.Second).Result()
+	}, 120*time.Second).Result()
 	if err != nil {
 		syncer.Log.Errorw("timeout waiting for remote syncer", "err", err)
 	}
