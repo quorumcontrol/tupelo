5577e4316801c8496f66faae67f51f43314948d1
Topper Bowers <topper@toppingdesign.com>
2019-03-15 14:32:36 +0100

    add a startjaeger

--- gossip3/tracing/tracing.go
+++ gossip3/tracing/tracing.go
@@ -1,10 +1,50 @@
 package tracing
 
 import (
+	"log"
+
 	"github.com/opentracing/opentracing-go"
+	"github.com/uber/jaeger-client-go"
+	jaegercfg "github.com/uber/jaeger-client-go/config"
+	jaegerlog "github.com/uber/jaeger-client-go/log"
+	"github.com/uber/jaeger-lib/metrics"
 	"go.elastic.co/apm/module/apmot"
 )
 
 func StartElastic() {
 	opentracing.SetGlobalTracer(apmot.New())
+}
+
+func StartJaeger() {
+	// Sample configuration for testing. Use constant sampling to sample every trace
+	// and enable LogSpan to log every span via configured Logger.
+	cfg := jaegercfg.Configuration{
+		Sampler: &jaegercfg.SamplerConfig{
+			Type:  jaeger.SamplerTypeConst,
+			Param: 1,
+		},
+		Reporter: &jaegercfg.ReporterConfig{
+			LogSpans: true,
+		},
+	}
+
+	// Example logger and metrics factory. Use github.com/uber/jaeger-client-go/log
+	// and github.com/uber/jaeger-lib/metrics respectively to bind to real logging and metrics
+	// frameworks.
+	jLogger := jaegerlog.StdLogger
+	jMetricsFactory := metrics.NullFactory
+
+	// Initialize tracer with a logger and a metrics factory
+	_, err := cfg.InitGlobalTracer(
+		"testNode",
+		jaegercfg.Logger(jLogger),
+		jaegercfg.Metrics(jMetricsFactory),
+	)
+	if err != nil {
+		log.Printf("Could not initialize jaeger tracer: %s", err.Error())
+		return
+	}
+	// defer closer.Close()
+
+	// continue main()
 }
