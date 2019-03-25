af98af6844a299232067bae5c0be19087f408de8
Topper Bowers <topper@toppingdesign.com>
2019-03-25 16:17:03 +0100

    up workers to 128

--- gossip3/actors/conflictset.go
+++ gossip3/actors/conflictset.go
@@ -74,7 +74,7 @@ func NewConflictSet(id string) *ConflictSet {
 	return c
 }
 
-const conflictSetConcurrency = 50
+const conflictSetConcurrency = 128
 
 func NewConflictSetWorkerPool(cfg *ConflictSetConfig) *actor.Props {
 	// it's important that this is a consistent hash pool rather than round robin
