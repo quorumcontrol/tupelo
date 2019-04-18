dd2d4f8ed76e3110d7d96e11f30c0864dceaa9ec
Arve Knudsen <arve.knudsen@gmail.com>
2019-04-18 15:49:35 +0200

    Add CircleCI config

--- scripts/build-within-docker.sh
+++ scripts/build-within-docker.sh
@@ -2,14 +2,8 @@
 set -eo pipefail
 
 mkdir -p ~/.ssh
-echo "$SSH_KEY" > ~/.ssh/id_rsa
-chmod 600 ~/.ssh/id_rsa
-eval `ssh-agent`
-ssh-add ~/.ssh/id_rsa
 git config --global url."ssh://git@github.com/".insteadOf "https://github.com/"
 ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
-./scripts/download-golangci-lint.sh
 
-go mod download
-golangci-lint run
+make lint
 go test -mod=readonly ./... -tags=integration
