e4a7076198f2f1339e53040f1a7f7379a52893e8
Brandon Westcott <brandon@quorumcontrol.com>
2019-11-11 14:55:53 -0500

    Support absolute path to notarygroup config file

--- nodebuilder/humanconfig_test.go
+++ nodebuilder/humanconfig_test.go
@@ -1,15 +1,46 @@
 package nodebuilder
 
 import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-
 	"github.com/stretchr/testify/require"
+
+	"github.com/BurntSushi/toml"
 )
 
 func TestTomlLoading(t *testing.T) {
 	c, err := TomlToConfig("testconfigs/basic.toml")
+	require.Nil(t, err)
+	assert.Len(t, c.NotaryGroupConfig.Signers, 2)
+	assert.Equal(t, c.BootstrapNodes, []string{"/ip4/172.16.238.10/tcp/34001/ipfs/16Uiu2HAm3TGSEKEjagcCojSJeaT5rypaeJMKejijvYSnAjviWwV5"})
+}
+
+func TestNotaryGroupAbsPath(t *testing.T) {
+	tomlBits, err := ioutil.ReadFile("testconfigs/basic.toml")
+	require.Nil(t, err)
+
+	var hc HumanConfig
+	_, err = toml.Decode(string(tomlBits), &hc)
+	require.Nil(t, err)
+
+	tmpfile, err := ioutil.TempFile("", "basic-with-full-ng-abs-path.toml")
+	require.Nil(t, err)
+	defer os.Remove(tmpfile.Name())
+
+	pwd, err := os.Getwd()
+	require.Nil(t, err)
+	hc.NotaryGroupConfig = filepath.Join(pwd, "testconfigs", hc.NotaryGroupConfig)
+
+	encoder := toml.NewEncoder(tmpfile)
+	err = encoder.Encode(hc)
+	require.Nil(t, err)
+	require.Nil(t, tmpfile.Close())
+
+	c, err := TomlToConfig(tmpfile.Name())
 	require.Nil(t, err)
 	assert.Len(t, c.NotaryGroupConfig.Signers, 2)
 	assert.Equal(t, c.BootstrapNodes, []string{"/ip4/172.16.238.10/tcp/34001/ipfs/16Uiu2HAm3TGSEKEjagcCojSJeaT5rypaeJMKejijvYSnAjviWwV5"})
