1feb594338d8aaa6ceacc86eae5496f4f335ce48
Topper Bowers <topper@toppingdesign.com>
2019-11-25 11:08:23 +0100

    add benchmark test for the validator - it's fast

--- gossip4/txvalidator_test.go
+++ gossip4/txvalidator_test.go
@@ -2,6 +2,7 @@ package gossip4
 
 import (
 	"context"
+	"sync"
 	"testing"
 	"time"
 
@@ -60,4 +61,59 @@ func TestTransactionValidator(t *testing.T) {
 	abr.Height = 1000
 	isValid = validator.validateAbr(ctx, &abr)
 	require.False(t, isValid)
+}
+
+// 25 Nov 2019 - Topper's MBP
+// BenchmarkTransactionValidator-12    	    7638	    194252 ns/op	  152368 B/op	    2016 allocs/op
+func BenchmarkTransactionValidator(b *testing.B) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	numMembers := 1
+	ts := testnotarygroup.NewTestSet(b, numMembers)
+	ng, nodes, err := newTupeloSystem(ctx, ts)
+	require.Nil(b, err)
+	require.Len(b, nodes, numMembers)
+
+	wg := &sync.WaitGroup{}
+
+	receiver := func(actorContext actor.Context) {
+		switch actorContext.Message().(type) {
+		case *services.AddBlockRequest:
+			wg.Done()
+		}
+	}
+
+	act := actor.EmptyRootContext.Spawn(actor.PropsFromFunc(receiver))
+	defer actor.EmptyRootContext.Stop(act)
+
+	validator, err := newTransactionValidator(logging.Logger("txvalidatortest"), ng, act)
+	require.Nil(b, err)
+
+	txs := make([]*pubsub.Message, b.N)
+	for i := 0; i < b.N; i++ {
+		abr := testhelpers.NewValidTransaction(b)
+		abrBits, _ := abr.Marshal()
+		txs[i] = &pubsub.Message{
+			Message: &pb.Message{
+				Data: abrBits,
+			},
+		}
+	}
+
+	peerID := peer.ID(nodes[0].p2pNode.Identity())
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		wg.Add(1)
+		// in the real system everything happens in parallel with a max of 1024
+		// We don't limit in the benchmark because we're assuming < 1024 TPS
+		go func(msg *pubsub.Message) {
+			isValid := validator.validate(ctx, peerID, msg)
+			if !isValid {
+				b.Fatal("valid wasn't true")
+			}
+		}(txs[i])
+	}
+	wg.Wait()
 }
