8b9bb0eba85c2eb074ab466a380f3fc9d809a75b
Brandon Westcott <brandon@quorumcontrol.com>
2019-11-27 15:44:36 -0500

    start ssh daemon in ci-gitsetup.sh if specified

--- scripts/ci-gitsetup.sh
+++ scripts/ci-gitsetup.sh
@@ -9,3 +9,11 @@ ssh-keyscan -t rsa github.com > github.pub
 diff <(ssh-keygen -lf github.pub) <(echo "2048 SHA256:nThbg6kXUpJWGl7E1IGOCspRomTxdCARLviKw6E5SY8 github.com (RSA)")
 cat github.pub >> ~/.ssh/known_hosts
 rm -f github.pub
+
+if [[ ! -z "$SSH_PRIVATE_KEY" ]]; then
+  mkdir -p ~/.ssh
+  echo "$SSH_PRIVATE_KEY" > ~/.ssh/id_rsa
+  chmod 600 ~/.ssh/id_rsa
+  eval "$(ssh-agent -s)" > /dev/null 2>&1
+  ssh-add ~/.ssh/id_rsa > /dev/null 2>&1
+fi
